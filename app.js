26c2fe448e9976a87e316b8672d0eb17f4f3df6c
Vid <weetopol@gmail.com>
2018-02-03 16:34:06 +0100

    RESTful api set up

---
+++ app.js
@@ -0,0 +1,109 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+app.use(bodyParser.json());
+
+Taxi = require('./models/taxi');
+Rent = require('./models/rent');
+
+//Connect to Mongoose
+mongoose.connect('mongodb://localhost/taxiapp');
+var db = mongoose.connection;
+
+// Route for the landing page
+app.get('/', function(req, res){
+    res.send('Ola taxi brother man!');
+});
+
+
+// -------------------------------
+// TAXIES
+// -------------------------------
+
+// get all taxies
+app.get('/api/taxies', function(req, res){
+    Taxi.getTaxies(function(err, taxies){
+        if(err) throw err;
+        res.json(taxies);
+    });
+})
+
+// create a taxi
+app.post('/api/taxies', function(req, res){
+    var taxi = req.body;
+    Taxi.addTaxi(taxi, function(err, taxi){
+        if(err) throw err;
+        res.json(taxi);
+    });
+})
+
+// get one taxi
+app.get('/api/taxies/:_id', function(req, res){
+    Taxi.getTaxiById(req.params._id, function(err, taxi){
+        if(err) throw err;
+        res.json(taxi);
+    });
+})
+
+// update a taxi
+app.put('/api/taxies/:_id', function(req, res){
+    var id = req.params._id; // v db je '_id'
+    var taxi = req.body;
+    Taxi.updateTaxi(id, taxi, {}, function(err, taxi){
+        if(err) throw err;
+        res.json(taxi);
+    });
+})
+
+// delete a taxi
+app.delete('/api/taxies/:_id', function(req, res){
+    var id = req.params._id; // v db je '_id'
+    Taxi.deleteTaxi(id, function(err, taxi){
+        if(err) throw err;
+        res.json(taxi);
+    });
+})
+
+// -------------------------------
+// RENTS
+// -------------------------------
+
+// get all rents
+app.get('/api/rents', function(req, res){
+    Rent.getRents(function(err, rents){
+        if(err) throw err;
+        res.json(rents);
+    });
+})
+
+// create a rent
+app.post('/api/rents', function(req, res){
+    var rent = req.body;
+    Rent.addRent(rent, function(err, rent){
+        if(err) throw err;
+        res.json(rent);
+    });
+})
+
+// get one rent by ID
+app.get('/api/rents/:_id', function(req, res){
+    Rent.getRentById(req.params._id, function(err, rent){
+        if(err) throw err;
+        res.json(rent);
+    });
+})
+
+// delete a rent
+app.delete('/api/rents/:_id', function(req, res){
+    var id = req.params._id; // v db je '_id'
+    Rent.deleteRent(id, function(err, rent){
+        if(err) throw err;
+        res.json(rent);
+    });
+})
+
+
+
+app.listen(3000);
+console.log('App running on port 3000')
---
+++ models/taxi.js
@@ -0,0 +1,90 @@
+var mongoose = require('mongoose');
+
+//Shema za taksi
+var taxiSchema = mongoose.Schema({
+    year: {
+        type: Number,
+        require: true,
+        default: function(){
+            return Math.floor(Math.random()*18+1990);
+        }
+    },
+    max_p: {
+        type: Number,
+        require: true,
+        default: function(){
+            return Math.floor(Math.random()*5+1);
+        }
+    },
+    max_s: {
+        type: Number,
+        require: true,
+        default: function(){
+            return Math.floor(Math.random()*90+150);
+        }
+    },
+    photo_url: { //random taxi photo
+        type: String,
+        require: true
+        /*default: function(){ 
+            return Math.floor(Math.random()*18+1990);
+        }*/
+    },
+    name: { //random iz nekega arraya
+        type: String,
+        require: true
+        /*default: function(){
+            return Math.floor(Math.random()*18+1990);
+        }*/
+    },
+    sum: {
+        type: Number,
+        require: true,
+        default: 0
+    },
+    history: {
+        type: Array,
+        require: true,
+        default: []
+    }
+});
+
+// Taxi objekt bo povsod dostopen
+var Taxi = module.exports = mongoose.model('Taxi', taxiSchema);
+
+// Taxies methods
+
+// get all
+module.exports.getTaxies = function(callback, limit){
+    Taxi.find(callback).limit(limit);
+};
+
+// find one
+module.exports.getTaxiById = function(id, callback){
+    Taxi.findById(id, callback);
+};
+
+// create a taxi
+module.exports.addTaxi = function(taxi, callback){ //taxi = objekt iz forme
+    Taxi.create(taxi, callback);
+};
+
+// update a taxi
+module.exports.updateTaxi = function(id, taxi, options, callback){ //taxi = nov objekt iz forme
+    var query = {_id: id};
+    var update = {
+        year: taxi.year,
+        max_p: taxi.max_p,
+        max_s: taxi.max_s,
+        sum: taxi.sum,
+        history: taxi.history,
+        name: taxi.name
+    }
+    Taxi.findOneAndUpdate(query, update, options, callback);
+};
+
+// delete a taxi
+module.exports.deleteTaxi = function(id, callback){ //taxi = objekt iz forme
+    var query = {_id: id};
+    Taxi.remove(query, callback);
+};
