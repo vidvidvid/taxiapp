0d300463bab8674207be32b6ba971fb392ea26b4
Vid <weetopol@gmail.com>
2018-02-21 09:33:24 +0100

    Cancel rent - action button working

--- client/services/taxies.js
+++ client/services/taxies.js
@@ -2,41 +2,10 @@ angular
     .module('myApp')
     .service('taxiesService', ['dataFactory', function (dataFactory, ) {
         var taxi = {};
-        taxiesService = {}
-
         var cancel = {name: 'Preklic', price: 500}
-        taxiesService.taxies = [];
+        taxiesService = {}
         taxiesService.taxi = {};
         taxiesService.taxi.history = [];
-
-        taxiesService.getTaxies = () => {
-            //console.log('X')
-            dataFactory.getTaxies().then(function(response){ //make a get req from this address
-                taxiesService.taxies = response.data; 
-            });
-        }
-
-        taxiesService.getTaxi = () => {
-            var id = $routeParams.id;
-            dataFactory.getTaxi(id).then(function(response){ 
-                taxiesService.taxi = response.data; //to moram uporabit, da dobim taxi
-            });
-        }
-
-        taxiesService.removeTaxi = (id) => {
-            dataFactory.removeTaxi(id).then(function(response){ 
-                //window.location.href='#!'; 
-            });
-        }
-
-        taxiesService.getTotal = (taxi) => {
-            var total = 0;
-            for(var i = 0; i < taxi.history.length; i++){ // deluje tudi z $scope.taxi.history.length
-                var rent = taxi.history[i];
-                if(rent.price) total += rent.price;
-            }
-            return total;
-        }
         
         taxiesService.cancelTaxi = (taxi, id) => {
             console.log('cancelling..')
@@ -45,9 +14,6 @@ angular
             dataFactory.updateTaxi(id, taxi).then(function(response){ 
             });
         }
-
-
-
 
         return taxiesService;
     }]);
