cc4f365ff953fd3943c272583468540b6c6e6694
Vid <weetopol@gmail.com>
2018-02-26 22:13:17 +0100

    Highchart taxies/time

--- client/controllers/header.js
+++ client/controllers/header.js
@@ -22,8 +22,7 @@ angular
         $scope.getTaxies = () => {
             dataFactory.getTaxies().then(function (response) {
                 $scope.taxies = response.data;
-                getSumOfAll();
-
+                updateViewData();
             });
         }
 
@@ -79,69 +78,13 @@ angular
             return total;
         }
 
-        currentlyRented = (taxies) => {
-            var count = 0;
-            for (var i = 0; i < taxies.length; i++) {
-                if (!taxies[i].available) count++;
-            }
-            return count;
-        }
-
-        lastHourSumAll = (taxies) => {
-            var sum = 0;
-            for (var i = 0; i < taxies.length; i++) {
-                sum += lastHourSumOne(taxies[i]);
-            }
-            return Math.round(sum / 60);
-        }
-
-        lastHourSumOne = (taxi) => {
-            var sum = 0;
-            var timeNow = Date.now();
-            if (taxi.history.length > 0) {
-                for (var i = 0; i < taxi.history.length; i++) {
-                    if (Math.floor((Date.now() - taxi.history[i].date) / 1000) <= 3600 && taxi.history[i].name !== 'Nakup') sum += taxi.history[i].price;
-                }
-                return sum;
-            }
-            return 0;
-        }
-
-        mostRented = (taxies) => {
-            var most = 0;
-            var times;
-            times = timesRented(taxies[0]);
-            for (var i = 1; i < taxies.length; i++) {
-                if (timesRented(taxies[i]) > times) {
-                    times = timesRented(taxies[i]);
-                    most = i;
-                }
-
-            }
-            return taxies[most].name;
-        }
-
-        timesRented = (taxi) => {
-            var count = 0;
-            if (taxi.history.length) {
-                for (var i = 0; i < taxi.history.length; i++) {
-                    if (taxi.history[i].name != 'Nakup' && taxi.history[i].name != 'Preklic') count++;
-                }
-                return count;
-            }
-            return 0;
-        }
-
-        getSumOfAll = () => {
+        updateViewData = () => {
             dataFactory.getTaxies().then(function (response) {
                 taxies = response.data;
                 $scope.gains = getTotalAll(taxies);
-                $scope.currentlyRented = currentlyRented(taxies);
-                $scope.lastHourSumAll = lastHourSumAll(taxies);
-                if (taxies.length) $scope.mostRented = mostRented(taxies);
                 var taxi = {};
                 for (var i = 0; i < taxies.length; i++) {
-                    if (check(taxies[i])) {
+                    if (checkValidity(taxies[i])) {
                         if (timeSpent(taxies[i]) > length(taxies[i])) {
                             taxi = taxies[i];
                             taxi.available = true;
@@ -153,7 +96,7 @@ angular
             console.log('Getting sum of all taxies');
         }
 
-        check = (taxi) => {
+        checkValidity = (taxi) => {
             return !(taxi.history.length === 0 || taxi.history[0].name === 'Preklici' || taxi.history[0].name === 'Nakup')
         }
 
@@ -166,6 +109,6 @@ angular
         timeSpent = (taxi) => {
             if (taxi.history[0].date) return Math.floor((Date.now() - taxi.history[0].date) / 1000);
         }
-
-        $interval(getSumOfAll, 10000);
+        
+        $interval(updateViewData, 10000);
     }]);
--- client/controllers/taxies.js
+++ client/controllers/taxies.js
@@ -53,11 +53,121 @@ angular
             dataFactory.updateTaxi(id, taxi).then(function (response) {});
         }
 
+        currentlyRented = (taxies) => {
+            var count = 0;
+            for (var i = 0; i < taxies.length; i++) {
+                if (!taxies[i].available) count++;
+            }
+            return count;
+        }
+
+        lastHourSumAll = (taxies) => {
+            var sum = 0;
+            for (var i = 0; i < taxies.length; i++) {
+                sum += lastHourSumOne(taxies[i]);
+            }
+            return Math.round(sum / 60);
+        }
+
+        lastHourSumOne = (taxi) => {
+            var sum = 0;
+            var timeNow = Date.now();
+            if (taxi.history.length > 0) {
+                for (var i = 0; i < taxi.history.length; i++) {
+                    if (Math.floor((Date.now() - taxi.history[i].date) / 1000) <= 3600 && taxi.history[i].name !== 'Nakup') sum += taxi.history[i].price;
+                }
+                return sum;
+            }
+            return 0;
+        }
+
+        mostRented = (taxies) => {
+            var most = 0;
+            var times;
+            times = timesRented(taxies[0]);
+            for (var i = 1; i < taxies.length; i++) {
+                if (timesRented(taxies[i]) > times) {
+                    times = timesRented(taxies[i]);
+                    most = i;
+                }
+
+            }
+            return taxies[most].name;
+        }
+
+        timesRented = (taxi) => {
+            var count = 0;
+            if (taxi.history.length) {
+                for (var i = 0; i < taxi.history.length; i++) {
+                    if (taxi.history[i].name != 'Nakup' && taxi.history[i].name != 'Preklic') count++;
+                }
+                return count;
+            }
+            return 0;
+        }
+
         var updateTaxies = () => {
-            console.log('Checking rent length')
+            console.log('Updating taxies')
             dataFactory.getTaxies().then(function (response) {
                 $scope.taxies = response.data;
+                $scope.currentlyRented = currentlyRented(taxies);
+                $scope.lastHourSumAll = lastHourSumAll(taxies);
+                if (taxies.length) $scope.mostRented = mostRented(taxies);
             });
         }
+
+        $scope.refreshData = () => {
+            dataFactory.getTaxies().then(function (response) {
+                console.log('osvezi');
+                taxiesHighchart();
+            });
+        }
+
+        getRentsArray = () => {
+            return new Promise(function(resolve, reject){
+                dataFactory.getTaxies().then(function (response){
+                    var rents = new Array(18);
+                    rents.fill(0);
+                    taxies = response.data;
+                    var count, date, rentStart, point;
+                    var timeNow = Math.floor(Date.now()/60000);
+                    var timeStart = timeNow - 180;
+    
+                    //fill the rents array with data
+                    for(var i = 0; i < taxies.length; i++){
+                        for(var j = 0; j < taxies[i].history.length; j++){
+                            rentStart = Math.floor(taxies[i].history[j].date/60000);
+                            if(rentStart<=timeStart) break;
+                            if(taxies[i].history[j].name!='Nakup' && taxies[i].history[j].name!='Preklic'){
+                                point = Math.floor((timeNow-rentStart)/10);
+                                rents[point]++;
+                            }
+                        }
+                    }
+                    resolve(rents);
+                })
+            })
+        }
+
+        taxiesHighchart = () => {
+            getRentsArray().then(function(array){
+                Highcharts.chart('container', {
+                    title: {
+                      text: 'Najem taksijev skozi čas'
+                    },
+              
+                    xAxis: {
+                      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+                      ]
+                    },
+              
+                    series: [{
+                      data: array
+                    }]
+                });
+            })
+        }
+        taxiesHighchart();
         $interval(updateTaxies, 2000);
     }]);
